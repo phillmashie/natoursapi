d6329233d74961803fb4471d8b1dd2e73944e1cc
Phillip Mashingaidze <phillmashie@gmail.com>
2025-04-22 12:49:05 +0200

    getting all tours endpoint

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint...');
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('Done');
 });
 
 const port = 3000;
