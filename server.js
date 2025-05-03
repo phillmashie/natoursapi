da673cf0845fd7767c0505a58cb687529e752f53
Phillip Mashingaidze <phillmashie@gmail.com>
2025-05-03 18:10:08 +0200

    modelling api

--- server.js
+++ server.js
@@ -17,39 +17,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: [true, 'The name must be unique'],
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR', err);
-  });
-
 // console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
