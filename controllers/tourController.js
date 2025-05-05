2da9aeabc3de342df2f3e77a387c8900fff0cac5
Phillip Mashingaidze <phillmashie@gmail.com>
2025-05-05 14:32:49 +0200

    filtering tours using fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,13 +12,24 @@ exports.getAllTours = async (req, res) => {
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
     if (req.query.sort) {
-      query = query.sort(req.query.sort);
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     // EXECUTE THE QUERY
