a8a5967676d21fcbbf76dc47a4d2c12eaf1ec2c2
Phillip Mashingaidze <phillmashie@gmail.com>
2025-05-07 18:35:30 +0200

    stats results and route implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -110,7 +110,27 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([]);
+    const stats = Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $min: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        stats,
+      },
+    });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
