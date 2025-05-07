b99b99fef839d1c6556e32b4f4d961265d062b14
Phillip Mashingaidze <phillmashie@gmail.com>
2025-05-07 18:42:29 +0200

    agregate pipilines : avgRating, avgPrice, minPrice, maxPrice

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -110,7 +110,7 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([
+    const stats = await Tour.aggregate([
       {
         $match: { ratingsAverage: { $gte: 4.5 } },
       },
